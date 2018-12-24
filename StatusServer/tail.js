b92f4255bb1206bcef0489a31ada15c2c8dc17a8
Connor Burns <ctrekker4@gmail.com>
2018-12-23 17:41:57 -0700

    Fix tail script

--- StatusServer/tail.js
+++ StatusServer/tail.js
@@ -3,15 +3,14 @@ readLastLines = require('read-last-lines');
 
 module.exports = function(args, callback) {
     readLastLines.read(args[0], args[1] || 10).then(function (lines) {
-        console.log(lines);
-        return {
+        callback({
             "indicators": [
                 {
                     "type": "console",
-                    "lines": lines
+                    "lines": lines.split('\r\n')
                 }
             ]
-        };
+        });
     }).catch(function(err) {
 
     });
